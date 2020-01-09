b2905a04187e704675d51e9f20e1750d18b25b77
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2020-01-09 17:40:06 +0100

    New feature: change password for a logged in user

--- pur_beurre/users/views.py
+++ pur_beurre/users/views.py
@@ -2,9 +2,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from catalog.models import Product
 from .forms import RegisterForm, ConnexionForm
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 def register(request):
