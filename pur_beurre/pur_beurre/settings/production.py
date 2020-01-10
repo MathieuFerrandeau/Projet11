490b3edc672893f4c9b31ad9d0871f02e474136a
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2020-01-10 16:28:20 +0100

    add production settings file

---
+++ pur_beurre/pur_beurre/settings/production.py
@@ -0,0 +1,16 @@
+from . import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+        'NAME': os.environ.get('DB_NAME') , # le nom de notre base de données créée précédemment
+        'USER': os.environ.get('DB_USER'), # attention : remplacez par votre nom d'utilisateur !!
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '',
+        'PORT': '5432',
+    }
+}
+
