645e14097c1f1d7d5a414d4b1a9503c95070941d
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2020-02-24 17:28:15 +0100

    add static root

--- pur_beurre/pur_beurre/settings/__init__.py
+++ pur_beurre/pur_beurre/settings/__init__.py
@@ -124,4 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
